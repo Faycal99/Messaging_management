cd3f4f12a763b6fc83eb6baf8d9bd4bb5df094b2
farouk_bhk <farouk.bouhaka@mf.gov.dz>
2025-02-25 11:23:47 +0100

    first commit

--- Mp/src/main/java/dgb/Mp/departement/Departement.java
+++ Mp/src/main/java/dgb/Mp/departement/Departement.java
@@ -1,4 +1,38 @@
 package dgb.Mp.departement;
 
+import dgb.Mp.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Departement {
+
+    // this entity created by demand of ait hadad to make all
+    // departement in the ministry can create there own system here
+    // like DGB space for couriel management and DGI space for there couriel management
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "depart_seq_gen", sequenceName = "depart_seq", allocationSize = 1)
+    private Long id;
+
+    private String departementName;
+
+    @OneToOne
+    @JoinColumn(name = "director_id")
+    private User director;
+
+    @OneToMany(mappedBy = "department")
+    private Set<User> employees;
+
+
 }
--- Mp/src/main/java/dgb/Mp/refreshToken/Dtos/RefreshTokenRequestDto.java
+++ Mp/src/main/java/dgb/Mp/refreshToken/Dtos/RefreshTokenRequestDto.java
@@ -1,4 +1,10 @@
-package dgb.Mp.refreshToken;
+package dgb.Mp.refreshToken.Dtos;
 
+import lombok.Getter;
+
+@Getter
 public class RefreshTokenRequestDto {
+
+    private String refreshToken;
+
 }
